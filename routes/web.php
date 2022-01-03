2c6781dfd49670185c284efdeabc1ea7b6f3a72b
saladinR <salaheddine.raiss@gmail.com>
2022-01-03 11:14:37 +0100

    add controller paiement

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\FileController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\avisController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\paiementController;
 
 /*
 |--------------------------------------------------------------------------
