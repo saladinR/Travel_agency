3a7108c267cfc6cbbdb43f1e91a5be8d140708a1
saladinR <salaheddine.raiss@gmail.com>
2022-01-04 10:23:35 +0100

    add modal in page home

--- resources/views/front-end/home.blade.php
+++ resources/views/front-end/home.blade.php
@@ -814,6 +814,20 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
         
 
         </script>
+        <script>
+            let copyText = document.querySelector(".copy-text");
+           
+               copyText.querySelector("button").addEventListener("click", function () {
+               let input = copyText.querySelector("input.text");
+               input.select();
+               document.execCommand("copy");
+               copyText.classList.add("active");
+               window.getSelection().removeAllRanges();
+               setTimeout(function () {
+                   copyText.classList.remove("active");
+               }, 2500);
+           });
+           </script>
 
     </body>
 </html>
